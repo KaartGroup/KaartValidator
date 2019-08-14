d6af12534d35a4d519cae433a0d5f8556507be09
Taylor Smock <taylor.smock@kaartgroup.com>
2019-08-14 15:19:33 -0600

    Don't increment date if the file hasn't changed
    Signed-off-by: Taylor Smock <taylor.smock@kaartgroup.com>

--- make.py
+++ make.py
@@ -5,6 +5,7 @@ import glob
 import shutil
 import git # I use GitPython
 import datetime
+import re
 
 PROJECT_DIRECTORY = "projects"
 MODULE_DIRECTORY = "modules"
@@ -13,9 +14,16 @@ def projectData(project):
     with open(project) as f:
         return json.load(f)
 
-def getVersion(filename):
+def getVersion(filename, prior_version = None):
     git_object = git.Repo(os.path.dirname(filename)).git
+    old_version = prior_version
+    split_version = []
+    if (prior_version is not None):
+        prior_version = prior_version.split("\"")[1]
+        split_version = prior_version.split("_")
     version = str(len(git_object.log(filename).split('\n')))
+    if (version == split_version[0]):
+        return split_version[0] + "_" + split_version[1]
     date = datetime.datetime.utcnow().strftime("%Y-%m-%d")
     return version + "_" + date
 
@@ -23,12 +31,20 @@ def getVersion(filename):
 def writeHeader(filename, data):
     if "meta" in data:
         meta = data['meta']
+        prior_version = None
+        with open(filename, 'r') as f:
+            line = f.readline()
+            while (line):
+                if (re.match("^\s*version:", line)):
+                    prior_version = line.split(":")[1].strip()
+                    break
+                line = f.readline()
         with open(filename, 'w') as f:
             f.write("meta\n")
             f.write("{\n")
             for key in meta:
                 f.write("  " + str(key) + ": \"" + str(meta[key]) + "\";\n")
-            f.write("  version: \"" + getVersion(filename) + "\";\n")
+            f.write("  version: \"" + getVersion(filename, prior_version = prior_version) + "\";\n")
             f.write("}\n")
         return True
     return False
